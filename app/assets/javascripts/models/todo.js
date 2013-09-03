9f976157f2c6c50dfd87c877f31a9f9cff4e0992
Jonathan Chrisp <jonathan.chrisp@gmail.com>
2013-09-03 19:48:50 +0100

    Removed fixtures no longer required

--- app/assets/javascripts/models/todo.js
+++ app/assets/javascripts/models/todo.js
@@ -2,21 +2,3 @@ KickstarterRails4EmberHeroku.Todo = DS.Model.extend({
   title: DS.attr('string'),
   isCompleted: DS.attr('boolean')
 });
-
-KickstarterRails4EmberHeroku.Todo.FIXTURES = [
- {
-   id: 1,
-   title: 'Learn Ember.js',
-   isCompleted: true
- },
- {
-   id: 2,
-   title: '...',
-   isCompleted: false
- },
- {
-   id: 3,
-   title: 'Profit!',
-   isCompleted: false
- }
-];
